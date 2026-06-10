695f6ae9b4591759e708ee31a588e75912a1d297
Laboratório FIAP <labsfiap@fiap.com.br>
2026-06-10 11:33:56 -0300

    aaaa

--- player.gd
+++ player.gd
@@ -1,11 +1,48 @@
-extends Node2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 300.0
+const JUMP_VELOCITY = -700.0
 
+@onready var player: CharacterBody2D = $"."
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Inverte o sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true	
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("walk")
+	else:
+			animated_sprite_2d.play("jump")
+
+	move_and_slide()
+	
+func die():
+# get_tree() — acessa o SceneTree, que é o gerenciador geral do jogo.
+# É por ele que você controla cenas, pausa o jogo, fecha o jogo, etc.
+# .reload_current_scene() — reinicia a cena atual do zero, como se
+# você tivesse fechado e reaberto ela.
+	get_tree().reload_current_scene()
